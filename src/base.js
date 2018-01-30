fd120fdaf7bf1436ffb1dc9e25fff02073d78532
Steve <hefr.steve@gmail.com>
2018-01-30 12:51:22 +0100

    firebase fixed

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base'
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyDncFk8mvNfflUbQ4HOBGjCym2q5kvUA_k",
-    authDomain: "add-friends-to-party.firebaseapp.com",
-    databaseURL: "https://add-friends-to-party.firebaseio.com"
+    apiKey: "AIzaSyDYYVMMsbpYMVR5kri6jKcOhb1xjnbKBCg",
+    authDomain: "friendslist-crud.firebaseapp.com",
+    databaseURL: "https://friendslist-crud.firebaseio.com"
 }
 
 const app = firebase.initializeApp(config)
